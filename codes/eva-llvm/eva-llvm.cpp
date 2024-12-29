0d3c8fdf028e349bb9c2abf1b27b2163947191b4
Vishwanath Kamath <vck281000@gmail.com>
2024-12-29 18:52:37 +0530

    lecture 7 done

--- codes/eva-llvm/eva-llvm.cpp
+++ codes/eva-llvm/eva-llvm.cpp
@@ -9,6 +9,10 @@ int main(int argc, char const *argv[])
 {
   // program to execute
   std::string program = R"(
+    // (var VERSION 42)
+    // (begin
+    //   (var VERSION "Hello")
+    //   (printf "Version: %s\n\n" VERSION))
     (printf "Version: %d\n\n" VERSION)
   )";
 
