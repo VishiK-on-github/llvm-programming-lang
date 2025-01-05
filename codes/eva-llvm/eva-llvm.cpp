eb4bb98b62ab91a82501c94f029a34f2ec691712
Vishwanath Kamath <vck281000@gmail.com>
2025-01-05 13:42:28 +0530

    lecture 16 done

--- codes/eva-llvm/eva-llvm.cpp
+++ codes/eva-llvm/eva-llvm.cpp
@@ -26,9 +26,37 @@ int main(int argc, char const *argv[])
     )
   )
 
-  (var p (new Point 10 20))
-  (printf "p.x = %d\n" (prop p x))
-  (printf "p.y = %d\n" (prop p y))
+  (class Point3D Point
+    (begin
+
+      (var z 100)
+
+      (def constructor (self x y z)
+        (begin
+          // ((method (super Point3D) constructor) self x y)
+          (set (prop self z) z)))
+
+      (def calc (self)
+        0)
+        // (+ ((method (super Point3D) calc) self) (prop self z)))
+
+  ))
+
+  (var p1 (new Point 10 20))
+  (var p2 (new Point3D 100 200 300))
+
+  (printf "p2.x = %d\n" (prop p2 x))
+  (printf "p2.y = %d\n" (prop p2 y))
+  (printf "p2.z = %d\n" (prop p2 z))
+
+  // (printf "Point3D.calc result = %d\n" ((method p2 calc) p2))
+
+  // (def check ((obj Point))
+  //   (begin
+  //     ((method obj calc) obj)))
+
+  // (check p1) // Point.calc
+  // (check p2) // Point3D.calc
 
   )";
 
